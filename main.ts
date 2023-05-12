a6303904af11434f6e34236a2d34962e9fd1f67b
mishimiauu <132573346+mishimiauu@users.noreply.github.com>
2023-05-12 08:32:40 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,12 @@
+basic.showIcon(IconNames.Heart)
+basic.showString("Hi 4ºESO")
+basic.showLeds(`
+    . . # . .
+    . . . . .
+    # . # . #
+    . . . . .
+    . . # . .
+    `)
+basic.forever(function () {
+	
+})
